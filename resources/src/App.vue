25ee9642a7fcbf74594e3f09cb96d34ed883258c
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-10-16 16:43:13 +0200

    Vue Implemented

---
+++ resources/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script setup>
+const title = "Laravel 9 + Vue 3 using Node v16.17.1"
+</script>
+
+<style scoped>
+    div {
+        text-align: center;
+    }
+</style>
