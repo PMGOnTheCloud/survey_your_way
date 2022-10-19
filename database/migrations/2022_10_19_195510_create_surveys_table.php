6550a313667914471bae01e443414b0d494a4011
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-10-19 22:28:16 +0200

    Modified migrations

--- database/migrations/2022_10_19_195510_create_surveys_table.php
+++ database/migrations/2022_10_19_195510_create_surveys_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
     {
         Schema::create('surveys', function (Blueprint $table) {
             $table->id();
+            $table->foreignId(\App\Models\User::class, 'user_id');
+            $table->string('title', 1000);
+            $table->string('slug', 1000);
+            $table->tinyInteger('status');
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->timestamp('expire_date')->nullable();
         });
     }
 
