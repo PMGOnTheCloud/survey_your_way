9f47856fdae21567c4c697e4fa56fa09d819d56c
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-10-25 20:53:28 +0200

    Added image upload and delete survey functionality

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\StoreSurveyRequest;
 use App\Http\Requests\UpdateSurveyRequest;
 use App\Models\Survey;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use App\Http\Resources\SurveyResource;
 
 class SurveyController extends Controller
@@ -29,8 +31,16 @@ class SurveyController extends Controller
      */
     public function store(StoreSurveyRequest $request)
     {
-        $result = Survey::create($request->validated());
-        return new SurveyResource($result);
+        $data = $request->validated();
+
+        if (isset($data['image'])) {
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+        }
+
+        $survey = Survey::create($data);
+
+        return new SurveyResource($survey);
     }
 
     /**
@@ -59,7 +69,20 @@ class SurveyController extends Controller
      */
     public function update(UpdateSurveyRequest $request, Survey $survey)
     {
-        $survey->update($request->validated());
+        $data = $request->validated();
+
+        if (isset($data['image'])) {
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+
+            if ($survey->image) {
+                $absolutePath = public_path($survey->image);
+                File::delete($absolutePath);
+            }
+        }
+
+        $survey->update($data);
+
         return new SurveyResource($survey);
     }
 
@@ -78,6 +101,47 @@ class SurveyController extends Controller
         }
 
         $survey->delete();
+
+        if ($survey->image) {
+            $absolutePath = public_path($survey->image);
+            File::delete($absolutePath);
+        }
+
         return response('', 204);
+    }
+
+    private function saveImage($image)
+    {
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
+            $image = substr($image, strpos($image, ',') + 1);
+            $type = strtolower($type[1]);
+
+            if (!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])) {
+                throw new Exception('Invalid image type');
+            }
+
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if ($image === false) {
+                throw new Exception('base64_decode Failed');
+            }
+
+        } else {
+            throw new Exception('Did not match data URI with image data');
+        }
+
+        $dir = 'images/';
+        $file = Str::random() . '.' . $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+
+        if (!File::exists($absolutePath)) {
+            File::makeDirectory($absolutePath, 0755, true);
+        }
+
+        file_put_contents($relativePath, $image);
+
+        return $relativePath;
     }
 }
--- resources/src/components/SurveyView.vue
+++ resources/src/components/SurveyView.vue
@@ -3,28 +3,34 @@
         <template v-slot:header>
             <div class="flex items-center justify-between">
                 <h1 class="text-3xl font-bold text-gray-900">
-                    {{ model.id ? model.title : "Create a Survey" }}
+                    {{ route.params.id ? model.title : "Create a Survey" }}
                 </h1>
+                <button v-if="route.params.id" type="button" @click="deleteSurvey()" class="py-2 px-3 text-white bg-red-500 rounded-md hover:bg-red-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 -mt-1 inline-block">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+                    Delete Survey
+                </button>
             </div>
         </template>
-        <pre>
-            {{model}}
-        </pre>
-        <form @submit.prevent="saveSurvey">
+        <div v-if="surveyLoading" class="flex justify-center">
+            Loading...
+        </div>
+        <form v-else @submit.prevent="saveSurvey">
             <div class="shadow sm:rounded-md sm:overflow-hidden">
                 <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
                     <label class="block text-sm font-medium text-gray-700">
                         Image
                     </label>
                     <div class="mt-1 flex items-center">
-                        <img v-if="model.image" :src="model.image" :alt="model.title" class="w-64 h-48 object-cover" />
+                        <img v-if="model.image_url" :src="model.image_url" :alt="model.title" class="w-64 h-48 object-cover" />
                         <span v-else class="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden bg-gray-100">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-[80%] w-[80%] text-gray-300">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
                         </span>
                         <button type="button" class="[&>*]:cursor-pointer relative overflow-hidden ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            <input type="file" class="absolute left-0 top-0 right-0 bottom-0 opacity-0" />
+                            <input type="file" @change="onImageChoose" class="absolute left-0 top-0 right-0 bottom-0 opacity-0" />
                             Change
                         </button>
                     </div>
@@ -89,7 +95,7 @@
 <script setup>
     import PageComponent from "../layouts/PageComponent.vue";
     import QuestionEditor from "./QuestionEditor.vue";
-    import { ref } from "vue";
+    import { ref, watch } from "vue";
     import { useRouter, useRoute } from "vue-router";
     import { useStore } from "vuex";
     import { v4 as uuidv4 } from "uuid";
@@ -98,20 +104,41 @@
     const route = useRoute();
     const store = useStore();
 
+    const surveyLoading = store.state.currentSurvey.loading;
+
     let model = ref({
         title: "",
         status: false,
         description: null,
-        image: null,
+        image_url: null,
         expire_date: null,
         questions: []
     });
 
+    watch(
+        () => store.state.currentSurvey.data,
+        (newVal, oldVal) => {
+            model.value = {
+                ...JSON.parse(JSON.stringify(newVal)),
+                status: newVal.status !=="draft",
+            }
+        }
+    );
+
     if (route.params.id) {
-        model.value = store.state.surveys.find(
-            (s) => s.id === parseInt(route.params.id)
-        );
+        store.dispatch('getSurvey', route.params.id);
     }
+
+    function onImageChoose(ev) {
+        const file = ev.target.files[0];
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            model.value.image = reader.result;
+            model.value.image_url = reader.result;
+        };
+        reader.readAsDataURL(file);
+    };
 
     function addQuestion(index) {
         const newQuestion = {
@@ -146,7 +173,17 @@
                 params: { id: data.data.id }
             });
         });
-    }
+    };
+
+    function deleteSurvey() {
+        if (confirm(`Are you sure you want to delete this survey? This survey can't be recovered`)) {
+            store.dispatch("deleteSurvey", model.value.id).then(() => {
+                router.push({
+                    name: "Surveys",
+                });
+            });
+        }
+    };
 
 </script>
 
